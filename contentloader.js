c53f74033f023ea51faa4692978e20b752e4bd98
toydotgame <toydotgame@gmail.com>
2024-02-29 20:06:32 +1000

    Basically stole ICCMC styles; Formatted a bit like old GCCC page

--- contentloader.js
+++ contentloader.js
@@ -1,72 +1,17 @@
 /*
  * AUTHOR: toydotgame
- * CREATED ON: 2024-02-16
- * Content-loading script so that I can update nav, header and footer,
- * etc all here and have it dynamically update across all pages.
+ * CREATED ON: 2024-02-29
+ * ICCMC content loader minus the `aside` element.
  */
 
 import {} from "/imgmodal.js";
 
-document.getElementById("navbar").innerHTML = `
-<a href="/" class="navlogo">LOGO</a>
-<a href="" class="nav">Plan</a>
+document.getElementById("nav").innerHTML = `
+<a href="/"><img id="logo" src="/media/logo.png"></a>
+|
+<a href="http://iccmc.toydotgame.net:8123/">Map</a>
+|
+<a href="/rules">Rules</a>
+|
+<a href="/about">About</a>
 `;
-
-var recentblogs = `
-<div id="recentblogs">
-<h1>Recent Blog Posts</h1>
-<ul>
-	<li><a href="/blog/2024-01-03_fs-measurements"><b>2024-01-03:</b> Digital Metric Unit Nomenclature for Dummies</a></li>
-	<li><a href="/blog/2024-01-01_web-design"><b>2024-01-01:</b> What's the deal with Modern Web Design?</a></li>
-	<li><a href="/blog/2024-01-01_spigot-dating-sucks"><b>2024-01-01:</b> Minecraft Server Crashing? Try This. – or (Another Reason) why Spigot Sucks</a></li>
-	<li><a href="/blog/2023-12-16_v5"><b>2023-12-16:</b> Site v5</a></li>
-	<li><a href="/blog/2023-04-10_toypack"><b>2023-04-10:</b> ToyPack v1.13.6</a></li>
-</ul>
-</div>
-`;
-
-var badges = `
-<img src="/media/badges/toydotgame.gif" width="88">
-<img src="/media/badges/win7.gif" width="88">
-<img src="/media/badges/invalidator.gif" width="88">
-<img src="/media/badges/vcss-blue.gif" width="88">
-<a href="http://www.wtfpl.net/"><img src="/media/badges/wtfpl.gif" width="88"></a>
-<a href="https://thardwardy.github.io/"><img src="/media/badges/thardwardy.gif" width="88"></a>
-<img src="/media/badges/2019.gif" width="88">
-<a href="https://cyber.dabamos.de/88x31/"><img src="/media/badges/88x31.gif" width="88"></a>
-<img src="/media/badges/abrowser.gif" width="88">
-<img src="/media/badges/anythingbut.gif" width="88">
-<a href="https://archlinux.org/"><img src="/media/badges/archlinux.gif" width="88"></a>
-<img src="/media/badges/cssdif.gif" width="88">
-<a href="https://www.dell.com/"><img src="/media/badges/dell.gif" width="88"></a>
-<a href="https://www.mozilla.org/en-US/firefox/new/"><img src="/media/badges/firefox3.gif" width="88"></a>
-<img src="/media/badges/gregg.gif" width="88">
-<a href="https://archive.org/donate"><img src="/media/badges/internetarchive.gif" width="88"></a>
-<img src="/media/badges/miku.gif" width="88">
-<a href="https://minecraft.net/"><img src="/media/badges/minecraft.gif" width="88"></a>
-<img src="/media/badges/minibanner.gif" width="88">
-<img src="/media/badges/right2repair.gif" width="88">
-<a href="https://yesterweb.org/no-to-web3/"><img src="/media/badges/roly-saynotoweb3.gif" width="88"></a>
-<img src="/media/badges/transnow2.gif" width="88">
-<a href="https://donate.wikimedia.org/><img src="/media/badges/wikipedia.gif" width="88"></a>
-`;
-export { badges };
-
-var footer = `
-<br>
-<a href="https://www.free-website-hit-counter.com/"><img src="https://www.free-website-hit-counter.com/c.php?d=6&id=160327&s=5" width="88"></a>
-<p>Website <copyleft></copyleft> 2024 toydotgame</p>
-`;
-
-/*
- * Footer replacement shouldn't run on Japanese-language pages. `/jp/contento.js` handles that.
- * However, this script is called and run by `contento.js`'s import statement.
- */
-if(!window.location.pathname.startsWith("/jp/")) {
-	try {
-		document.getElementById("footer").innerHTML = "<hr>" + recentblogs + badges + footer;
-	} catch {
-		document.getElementById("blogfooter").innerHTML = "<hr>" + badges + footer;
-		document.getElementById("blogfooter").id = "footer";
-	}
-}
--- template.php
+++ template.php
@@ -3,16 +3,16 @@
 <!--
 	AUTHOR: toydotgame
 	CREATED ON: 2024-02-15
-	Template page.
+	Generic page.
 -->
 
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="/styles.css">
-		<title>TEMPLATE_TITLE | City of Gold Coast</title>
+		<title>TEMPLATE_TITLE - ICCMC</title>
 		<meta property="og:type" content="article">
-		<meta property="og:site_name" content="City of Gold Coast">
+		<meta property="og:site_name" content="ICCMC">
 		<meta property="og:title" content="TEMPLATE_TITLE">
 		<meta property="og:description" content="TEMPLATE_DESC">
 		<meta name="viewport" content="width=device-width">
@@ -24,12 +24,19 @@
 			<h1>Title</h1>
 			<h2>Heading 2</h2>
 			<h3>Heading 3</h3>
-			<hr>
 			<p>Lorem ipsum dolor sit amet. <code>code text</code></p>
 			<small>small</small>
 			<br>
 			<img src="https://placekitten.com/300/300" width="25%">
+			<table>
+				<tr><th>Col 1</th><th>Col 2</th></tr>
+				<tr><td>Title</td><th>Value</th></tr>
+				<tr><td>Title</td><th>Value</th></tr>
+				<tr><td>Title</td><th>Value</th></tr>
+				<tr><td>Title</td><th>Value</th></tr>
+				<tr><td>Title</td><th>Value</th></tr>
+			</table>
 		</div></div>
-		<div id="aside"></div>
+		<div id="footer"><p>&copy; 2024 Gold Coast City Council</p></div>
 	</body>
 </html>
